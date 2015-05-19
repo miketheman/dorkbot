84be2c0366ceccc8d8421311959e78a0342124fc
Mike Fiedler <miketheman@gmail.com>
2015-05-19 08:10:17 -0400

    first addition of local custom dork handlers

---
+++ lib/dorks.rb
@@ -0,0 +1,25 @@
+module Lita
+  # It's a Handler, not anything else!
+  module Handlers
+    # And it's for our beloved Dorks!
+    class Dorks < Handler
+      # Inline response callback
+      route(/foo/) do |response|
+        response.reply 'bar'
+      end
+
+      # definition-based callback
+      route('baz', :quux)
+      def quux(response)
+        response.reply 'quux'
+      end
+
+      # Only respone to a command when spken to directly
+      route(/crazy/, command: true) do |response|
+        response.reply "You think I'M crazy? YOU'RE CRAZY!"
+      end
+    end
+
+    Lita.register_handler(Dorks)
+  end
+end
